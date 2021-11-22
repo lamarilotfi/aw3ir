67ce9e3d4e15910735762491faf00a52183f2d5b
lamarilotfi <92303803+lamarilotfi@users.noreply.github.com>
2021-11-22 15:26:49 +0100

    tp3

---
+++ tp3/js/form-validation.js
@@ -0,0 +1,10 @@
+window.onload = function () {   // ce code est exécuter une fois que toute la page est téléchargée par le navigateur
+    // voir plus : https://www.w3schools.com/js/js_htmldom.asp
+     console.log( "DOM ready!" );
+     
+     // Y mettre le code Javascript pour valider tous les champs du formulaire
+     document.querySelector("form").addEventListener("submit", function (event) {
+        event.preventDefault();
+        console.log("form submitted!");
+      });
+ };
